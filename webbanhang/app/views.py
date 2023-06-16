ae516edc7bd25c5327eb88a6899ede48c8ff70f5
Hung <bigbug0942@gmail.com>
2023-06-16 17:24:16 +0700

    16/5/2023

--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -3,5 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'app/home.html')
+def base(request):
+    context = {}
+    return render(request, 'app/home.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'app/checkout.html', context)
